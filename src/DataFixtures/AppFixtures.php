c8401660ec2859fef409e5b33de14dbbcca212ab
nadir <nadir.fouka@gmail.com>
2020-11-25 10:22:58 +0100

    Add Article Fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use joshtronic\LoremIpsum;
+
+
+class AppFixtures extends Fixture
+{
+
+        public function load(ObjectManager $manager)
+    {
+
+
+       $lipsum = new LoremIpsum() ;
+
+        for ($i = 0; $i < 50; $i++) {
+            $customer = new Article();
+            $customer->setTitle( $lipsum->words(10) );
+            $customer->setContent( $lipsum->paragraphs(50) ) ;
+            $manager->persist($customer);
+        }
+
+        $manager->flush();
+    }
+
+
+}
